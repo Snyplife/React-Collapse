b5031c632a9b57cc1ce93784cc111ff079f0dabf
Snyplife <Nassimbelferoum@gmail.com>
2017-11-13 17:08:13 +0100

    first commit

---
+++ src/js/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import image from '../images/expand-vertical-4.svg';
+import Collapsible from './Collapsible';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <header>
+                    <img src={image} />
+                    <h1>Collapsible Content</h1>
+                </header>
+                <div className="content">
+                    <div className="panel-group">
+                      <Collapsible title="Top Movies">
+                        <p>Blade Runner</p>
+                      </Collapsible>
+                      <Collapsible title="Top Series">
+                        <p>Anim pariatur cliche reprehenderit,
+                           enim eiusmod high life accusamus terry richardson ad squid.
+                           3 wolf moon officia aute.</p>
+                      </Collapsible>
+                      <Collapsible title="Top Anime">
+                        <p>Anim pariatur cliche reprehenderit,
+                           enim eiusmod high life accusamus terry richardson ad squid.
+                           3 wolf moon officia aute.</p>
+                      </Collapsible>
+
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+export default App;
